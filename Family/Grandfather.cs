d61e68648ace4528b15a4e39eb05275d5df228ca
Githubsacri27 <rsacri27@hotmail.com>
2024-10-24 20:43:57 +0200

    Agregar archivos de proyecto.

---
+++ Family/Grandfather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family
+{
+    public class Grandfather
+    {
+
+        public string Name { get; set; }
+        protected int Age { get; set; }
+        private string FavoriteColor { get; set; }
+
+        public Grandfather(string name, int age, string favoriteColor)
+        {
+            Name = name;
+            Age = age;
+            FavoriteColor = favoriteColor;
+        }
+
+        public string GetFavoriteColor() => FavoriteColor;
+        public void SetFavoriteColor(string value) => FavoriteColor = value;
+    }
+
+}
